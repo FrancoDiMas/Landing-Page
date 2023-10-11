057e35d4532de2ce0488ac11946a3cfb5f8b563a
FrancoDiMas <francodimascio@hotmail.com>
2023-10-11 15:45:10 -0400

    fixing

--- js/scriptModificado.js
+++ js/scriptModificado.js
@@ -1,9 +1,8 @@
+// Alert Message For specific page
 window.onload = function() {
     var esTelefono = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     if (esTelefono) {
-      alert("¡Bienvenido, esta página está diseñada unicamente para equipos de escritorios (PC/Laptop), de esta manera puedas ver el mismo modelo anterior con diferentes colores, de lo contrario no podrás verla correctamente.!");
+        alert("¡Bienvenido, esta página está diseñada unicamente para equipos de escritorios (PC/Laptop), de esta manera puedas ver el mismo modelo anterior con diferentes colores, de lo contrario no podrás verla correctamente.!");
+        window.location.replace("https://francodimas.github.io/GitHub-Portfolio/");
     }
 };
-
-// Alert Message For specific page
-// alert("Esta página está diseñada unicamente para equipos de escritorios (PC/Laptop), de esta manera puedas ver el mismo modelo anterior con diferentes colores, de lo contrario no podrás verla correctamente.")
