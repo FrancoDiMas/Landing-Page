2fcde6c5ac856802d22d69dc7798a3e3cf686563
FrancoDiMas <francodimascio@hotmail.com>
2023-10-11 15:13:08 -0400

    Aviso en la página con colores diferentes.

---
+++ js/scriptModificado.js
@@ -0,0 +1,2 @@
+// Alert Message For specific page
+alert("Esta página está diseñada unicamente para equipos de escritorios (PC/Laptop), de esta manera puedas ver el mismo modelo anterior con diferentes colores.")
