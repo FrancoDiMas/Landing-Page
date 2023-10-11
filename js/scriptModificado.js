87e072b3b078eedce361e360e5dffa0a52e6f05a
FrancoDiMas <francodimascio@hotmail.com>
2023-10-11 15:26:15 -0400

    Alerta teléfono

--- js/scriptModificado.js
+++ js/scriptModificado.js
@@ -1,2 +1,9 @@
+window.onload = function() {
+    var esTelefono = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    if (esTelefono) {
+      alert("¡Bienvenido, esta página está diseñada unicamente para equipos de escritorios (PC/Laptop), de esta manera puedas ver el mismo modelo anterior con diferentes colores, de lo contrario no podrás verla correctamente.!");
+    }
+};
+
 // Alert Message For specific page
-alert("Esta página está diseñada unicamente para equipos de escritorios (PC/Laptop), de esta manera puedas ver el mismo modelo anterior con diferentes colores.")
+// alert("Esta página está diseñada unicamente para equipos de escritorios (PC/Laptop), de esta manera puedas ver el mismo modelo anterior con diferentes colores, de lo contrario no podrás verla correctamente.")
