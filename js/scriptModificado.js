022a8b71c5d4f2abe725c210bce57d07c97be9d4
FrancoDiMas <francodimascio@hotmail.com>
2023-10-13 14:52:14 -0400

    fixing

--- js/scriptModificado.js
+++ js/scriptModificado.js
@@ -2,6 +2,6 @@
 window.onload = function() {
     var esTelefono = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     if (esTelefono) {
-      alert("¡Bienvenido, esta página está diseñada unicamente para equipos de escritorios (PC/Laptop), de esta manera puedas ver el mismo modelo anterior con diferentes colores, de lo contrario no podrás verla correctamente.!");
+      alert("¡Bienvenido, esta página está diseñada unicamente para equipos de escritorios (PC/Laptop), de esta manera podrás ver el mismo modelo anterior con diferentes colores, de lo contrario no podrás verla correctamente.!");
     }
 };
